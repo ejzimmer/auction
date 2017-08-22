53c900d92fb2c18520828ebcf8dde994dfb195d5
Erin Zimmer <erin.zimmer@auspost.com.au>
2017-08-23 08:59:53 +1000

    app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { SearchComponent } from './search/search.component';
+import { StarsComponent } from './stars/stars.component';
+import { ProductService } from './product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductItemComponent,
+    SearchComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
