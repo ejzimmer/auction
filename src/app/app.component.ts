0f28df60553232bc43b028b36feb960c0265d7cc
erinz <erin.zimmer@auspost.com.au>
2017-08-25 08:23:43 +1000

    more stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { Product, ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'app';
+export default class AppComponent {
+  products: Array<Product> = [];
+
+  constructor(private productService: ProductService) {
+    this.products = this.productService.getProducts();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import AppComponent from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
