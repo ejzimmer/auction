e794874f45f6463f7de6ce89114687b691775c48
Erin Zimmer <erin.zimmer@auspost.com.au>
2017-08-26 17:56:45 +1000

    train

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { StarsComponent } from '../stars/stars.component';
+import { Product } from '../product.service';
 
 @Component({
-  selector: 'app-product-item',
+  selector: 'auction-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent implements OnInit {
 
+  @Input() product: Product;
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-stars',
+  selector: 'auction-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
+  @Input() count: number = 5;
+  @Input() rating: number = 0;
+  stars: boolean[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    for (let i = 0; i < this.count; i++) {
+      this.stars.push(i < this.rating);
+    }
   }
 
 }
